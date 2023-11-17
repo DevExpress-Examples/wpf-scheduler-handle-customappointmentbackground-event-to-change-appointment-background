0cc183d2130495819f1bbd86d5737d4cb4fa3a79
DevExpressExampleBot <fakeEmail>
2023-11-17 15:28:12 +0400

    Source auto update [skip ci]

--- VB/CustomAppointmentBackground/MainViewModel.vb
+++ VB/CustomAppointmentBackground/MainViewModel.vb
@@ -1,11 +1,12 @@
-﻿#Region "#viewmodel"
-Imports System
 Imports System.Collections.ObjectModel
 
 Namespace CustomAppointmentBackground
+
     Public Class MainViewModel
-        Public Overridable Property Doctors() As ObservableCollection(Of Doctor)
-        Public Overridable Property Appointments() As ObservableCollection(Of MedicalAppointment)
+
+        Public Overridable Property Doctors As ObservableCollection(Of Doctor)
+
+        Public Overridable Property Appointments As ObservableCollection(Of MedicalAppointment)
 
         Protected Sub New()
             CreateDoctors()
@@ -13,16 +14,11 @@ Namespace CustomAppointmentBackground
         End Sub
 
         Private Sub CreateDoctors()
-            Doctors = New ObservableCollection(Of Doctor)()
-            Doctors.Add(Doctor.Create(Id:=1,Name:="Stomatologist"))
-            Doctors.Add(Doctor.Create(Id:=2, Name:="Ophthalmologist"))
-            Doctors.Add(Doctor.Create(Id:=3, Name:="Surgeon"))
+            Doctors = New ObservableCollection(Of Doctor) From {Doctor.Create(Id:=1, Name:="Stomatologist"), Doctor.Create(Id:=2, Name:="Ophthalmologist"), Doctor.Create(Id:=3, Name:="Surgeon")}
         End Sub
+
         Private Sub CreateMedicalAppointments()
-            Appointments = New ObservableCollection(Of MedicalAppointment)()
-            Appointments.Add(MedicalAppointment.Create(StartTime := Date.Now.Date.AddHours(10), EndTime := Date.Now.Date.AddHours(11), DoctorId := 1, Notes := "", Location := "101", PatientName := "Dave Murrel", InsuranceNumber := "111111111", FirstVisit := True, AllDay:=False))
-            Appointments.Add(MedicalAppointment.Create(StartTime:= Date.Now.Date.AddHours(11), EndTime:= Date.Now.Date.AddHours(12), DoctorId:= 2, Notes:= "", Location:= "", PatientName:= "Day-off", InsuranceNumber:= "111111111", FirstVisit:= True, AllDay:= True))
+            Appointments = New ObservableCollection(Of MedicalAppointment) From {MedicalAppointment.Create(StartTime:=Date.Now.Date.AddHours(10), EndTime:=Date.Now.Date.AddHours(11), DoctorId:=1, Notes:="", Location:="101", PatientName:="Dave Murrel", InsuranceNumber:="111111111", FirstVisit:=True, AllDay:=False), MedicalAppointment.Create(StartTime:=Date.Now.Date.AddHours(11), EndTime:=Date.Now.Date.AddHours(12), DoctorId:=2, Notes:="", Location:="", PatientName:="Day-off", InsuranceNumber:="111111111", FirstVisit:=True, AllDay:=True)}
         End Sub
     End Class
 End Namespace
-#End Region ' #viewmodel
--- VB/CustomAppointmentBackground/MainWindow.xaml.vb
+++ VB/CustomAppointmentBackground/MainWindow.xaml.vb
@@ -1,40 +1,23 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-Imports System.Windows
-Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 Imports DevExpress.Xpf.Core
 Imports DevExpress.Xpf.Scheduling
 Imports DevExpress.XtraScheduler
+Imports System.Windows.Media
 
 Namespace CustomAppointmentBackground
-    ''' <summary>
-    ''' Interaction logic for MainWindow.xaml
-    ''' </summary>
-    Partial Public Class MainWindow
-        Inherits DXWindow
+
+    Public Partial Class MainWindow
+        Inherits ThemedWindow
 
         Public Sub New()
-            InitializeComponent()
+            Me.InitializeComponent()
         End Sub
 
-        #Region "#CustomAppointmentBackground"
         Private Sub SchedulerControl_CustomAppointmentBackground(ByVal sender As Object, ByVal e As CustomAppointmentBackgroundEventArgs)
-            'Change the recurrent appointments' background to blanched almond color,
-            'and other appointments - to rosy brown 
+            'Change the recurrent appointment background to blanched almond color,
+            'and background of other appointments - to rosy brown:
             e.Background = If(e.AppointmentItem.Type = AppointmentType.Occurrence, New SolidColorBrush(Colors.BlanchedAlmond), New SolidColorBrush(Colors.RosyBrown))
-
-            'Change all-day appointments' background:
+            'Change the all-day appointment background:
             e.Background = If(e.AppointmentItem.AllDay = True, New SolidColorBrush(Colors.Tomato), e.Background)
         End Sub
-        #End Region ' #CustomAppointmentBackground
     End Class
 End Namespace
